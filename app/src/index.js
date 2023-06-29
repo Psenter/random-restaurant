4252ed907698540cce010ba09771607b1bd15016
Parker S <111610335+Psenter@users.noreply.github.com>
2023-06-29 18:03:06 -0400

    Dinner now displays.

--- app/src/index.js
+++ app/src/index.js
@@ -8,11 +8,13 @@ import Header from './routes/Header';
 import Navbar from './routes/Navbar';
 import Appetizers from './routes/Appetizers';
 import Lunch from './routes/Lunch';
+import Dinner from './routes/Dinner';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <Navbar />
+    <Dinner />
   </React.StrictMode>
 );
---
+++ app/src/routes/Dinner.js
@@ -0,0 +1,43 @@
+//imports what I need
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+//exports my function
+export default function Dinner() {
+  //sets state to the data
+  const [dinner, setDinner] = useState([]);
+
+  
+  useEffect(() => {
+    //makes the call to the API
+    axios.get("https://www.jsonkeeper.com/b/MDXW").then((response) => {
+      //sets setAppetizers to the data from API
+      setDinner(response.data);
+    });
+  }, []);
+
+  //filters out all data that isnt 
+  const dinnerItems = dinner.filter((item) => item.category === "Dinner");
+
+  //returns the displayed data
+  return (
+    <>
+      <h1 className="text-center text-color-lb pt-5">Dinner</h1>
+      <div className="row">
+        {/* maps through the data array and puts it into divs */}
+        {dinnerItems.map((item) => (
+          <div
+            key={item.id}
+            className="col-6 text-center text-color-lb pt-5 ps-5 pe-5"
+          >
+            <h2 className="card-title">{item.title}</h2>
+            <h4>Category: {item.category}</h4>
+            <h5>Description: {item.description}</h5>
+            <h6>{item.price}</h6>
+            <br />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
